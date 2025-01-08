caea5ed3b91f1c08cfc196ddaa816bcfa87764b3
Youssef Nahdi <youssefnahdi95@gmail.com>
2025-01-08 19:30:37 +0100

    Create Project Structure

---
+++ SecureChat/src/chatapp/ChatAppLauncher.java
@@ -0,0 +1,10 @@
+package chatapp;
+
+public class ChatAppLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SecureChat/src/chatapp/ChatPeer.java
@@ -0,0 +1,5 @@
+package chatapp;
+
+public class ChatPeer {
+
+}
